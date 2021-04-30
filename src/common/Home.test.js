6c89f0190c1efa8dc75470a8b89f597a885371da
Dx <turbowars@gmail.com>
2021-04-30 07:42:13 -0500

    install Jest

---
+++ src/common/Home.test.js
@@ -0,0 +1,9 @@
+const sum = require('./Home');
+import useFetch from "./useFetch";
+
+
+describe('Fetching Data test', () => {
+  test('should be equal to 1', () => {
+    expect(1).toBe(1)
+  })
+})
